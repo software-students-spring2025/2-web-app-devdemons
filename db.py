705159119af812678fd4224aef529a7d5fbccf9a
a-wyndham1 <apollo_wyndham@yahoo.com>
2025-02-25 08:52:43 -0500

    Create csv with national park data and load

---
+++ db.py
@@ -0,0 +1,33 @@
+import csv
+import re
+
+def load_parks():
+    '''
+    Loading our National Park collection into our MongoDB database from a csv file
+    The csv is sourced from https://www.kaggle.com/datasets/thedevastator/the-united-states-national-parks/data which is  licensed as public 
+    The data on whether each park has an entrance fee comes from https://www.nps.gov/aboutus/entrance-fee-prices.htm?park=&state=&entrancePassRequired=&timedEntry=&page=1&parking=
+    For future reference the associated images for each park are stored in the static/images folder with the name being park_name.
+    '''
+    src = 'static/db/national_parks.csv'
+    file = csv.reader(open(src,'r'))
+    headers = next(file)
+    headers = [headers[1], headers[3],headers[5], headers[7],headers[8]]
+    parks = [[l[1], l[3],l[5], l[7],l[8]] for l in file]
+
+    for p in parks: #loading a json style object that then can be put into mongoDB once thats set up 
+        p_name, p_state, p_size, desc, fee = p
+        state = re.match('([a-zA-Z\s.&]*?)[\d]', p_state)[1]
+
+        if '&' in state:
+            state=state.replace('&', ', ')
+
+        park = {'name':p_name,
+                'state':state,
+                'size': p_size,
+                'fee': bool(fee)}
+        
+        # adding to mongodb
+        # db.national_parks.insert_one(park)
+
+if __name__ == "__main__":
+    load_parks()
